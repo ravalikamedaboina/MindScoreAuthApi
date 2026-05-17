2aeb4a2490b5d0f28513b4b6fb0104127e4d580f
Ravalika Raj <rajravalika19@gmail.com>
2026-05-17 11:59:56 -0500

    fixing swagger

--- MindScoreApi/Program.cs
+++ MindScoreApi/Program.cs
@@ -19,11 +19,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
